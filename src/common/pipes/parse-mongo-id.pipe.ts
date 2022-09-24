e0dff908bb45bd7e24fe1d917708e096528fffda
Facundo de la Torre <facu17294@gmail.com>
2022-09-23 23:16:42 -0300

    primer commit

---
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -0,0 +1,10 @@
+import { ArgumentMetadata, Injectable, PipeTransform,BadRequestException } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata) {
+    if(!isValidObjectId(value)) throw new BadRequestException('No es un ID valido')
+    return value;
+  }
+}
